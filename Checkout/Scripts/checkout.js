71f2775e6ddb4a6b1789d36f2f39f27a22a2a4c1
TusharKadam7671 <kadamtushar080@gmail.com>
2022-07-23 00:19:23 +0530

    alert is working

--- Checkout/Scripts/checkout.js
+++ Checkout/Scripts/checkout.js
@@ -3,3 +3,30 @@ let open_manual=()=>{
         document.getElementById("manual").style.display="block";
 }
 document.getElementById("open_manual").addEventListener("click",open_manual);
+
+
+
+let submit=()=>{
+        if(
+                document.getElementById("full_name").value!="" &&
+                document.getElementById("address").value!="" &&
+                document.getElementById("apartment").value!="" &&
+                document.getElementById("town_city").value!="" &&
+                document.getElementById("state").value!="" &&
+                document.getElementById("zipcode").value!="" &&
+                document.getElementById("contact").value!="" &&
+                document.getElementById("name_on_card").value!="" &&
+                document.getElementById("card_number").value!="" &&
+                document.getElementById("cvv_i").value!=""
+        ){
+                alert("Order Placed Successfully!");
+                window.location.href = "../index.html";
+                localStorage.setItem("Cart_data", JSON.stringify([]));
+        } else {
+                alert("Please fill all the required information");
+              }
+          
+}
+
+
+document.getElementById("button").addEventListener("click",submit)
